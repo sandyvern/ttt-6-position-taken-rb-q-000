2584c7c8497d949f9c53286a115aef8237e6f8c9
Sandy <SVern99@aol.com>
2016-05-09 21:42:56 +0000

    nitrous-backup at Mon May  9 21:42:55 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, location)
-  !(board[location] == " " || board[location] == "" || board[location] == nil)
+  !(board[location] == " " || board[location] == "" || board[location] == nil)  #board[location] is not eq. to " " or "" or nil
 end
 
-=begin
-if board[] != " "
-    false
-=end
